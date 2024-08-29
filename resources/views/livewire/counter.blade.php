30e9eaba70fdb8d74025f4142c5da9940ce36944
YousefHassan52 <yousef52hm@gmail.com>
2024-08-30 00:07:24 +0300

    bs kda 5las 5lsna

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -2,9 +2,9 @@
     <div class="text-center">
         <h1>Livewire Counter</h1>
         <div class="d-flex justify-content-center align-items-center mt-4">
-            <button type="button" class="btn btn-danger">-</button>
-            <div class="mx-3" style="width: 50px; text-align: center;">0</div>
-            <button class="btn btn-primary">+</button>
+            <button type="button" class="btn btn-danger" wire:click="minus">-</button>
+            <div class="mx-3" style="width: 50px; text-align: center;">{{$counter}}</div>
+            <button class="btn btn-primary" wire:click="plus">+</button>
         </div>
     </div>
 </div>
